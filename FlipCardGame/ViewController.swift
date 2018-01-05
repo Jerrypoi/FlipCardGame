601ded659ac3aa442c18ed178a22757084b0d151
Jerrypoi <jerry.poi.sz@gmail.com>
2018-01-05 18:57:30 +0800

    Initial Commit

---
+++ FlipCardGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlipCardGame
+//
+//  Created by Jerry Zhang on 05/01/2018.
+//  Copyright © 2018 Jerry Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
