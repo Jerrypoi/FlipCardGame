79c4ec90318c210c9f41c01996e4ffb25cb62748
Jerrypoi <jerry.poi.sz@gmail.com>
2018-01-12 23:19:34 +0800

    Add card file

---
+++ FlipCardGame/Card.swift
@@ -0,0 +1,13 @@
+//
+//  Card.swift
+//  FlipCardGame
+//
+//  Created by Jerry Zhang on 12/01/2018.
+//  Copyright © 2018 Jerry Zhang. All rights reserved.
+//
+
+import Foundation
+class Card {
+    var identifier:Int
+    
+}
